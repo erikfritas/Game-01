8af77622bde3462b2f40f41800c6f2d93d13d8b0
eriklutiel <74334533+eriklutiel@users.noreply.github.com>
2021-04-25 01:51:44 -0300

    Update Sprites.py

--- spritesheet/Sprites.py
+++ spritesheet/Sprites.py
@@ -4,16 +4,13 @@ import pygame as pyg
 class SpriteSheet:
 
     def __init__(self, filename):
-        """Load the sheet."""
         try:
             self.sheet = pyg.image.load(filename).convert()
         except pyg.error as e:
-            print(f"Unable to load spritesheet image: {filename}")
+            print(f"Não é possível carregar a imagem da spritesheet: {filename}")
             raise SystemExit(e)
 
     def image_at(self, rectangle, colorkey=None):
-        """Load a specific image from a specific rectangle."""
-        # Loads image from x, y, x+offset, y+offset.
         rect = pyg.Rect(rectangle)
         image = pyg.Surface(rect.size).convert()
         image.blit(self.sheet, (0, 0), rect)
@@ -24,11 +21,9 @@ class SpriteSheet:
         return image
 
     def images_at(self, rects, colorkey=None):
-        """Load a whole bunch of images and return them as a list."""
         return [self.image_at(rect, colorkey) for rect in rects]
 
     def load_strip(self, rect, image_count, colorkey=None):
-        """Load a whole strip of images, and return them as a list."""
         tups = [(rect[0]+rect[2]*x, rect[1], rect[2], rect[3])
                 for x in range(image_count)]
         return self.images_at(tups, colorkey)
